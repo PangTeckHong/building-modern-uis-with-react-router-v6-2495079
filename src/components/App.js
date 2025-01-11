ff51364d2d51ec6e1d608d94e2bf16b018de9985
Pang Teck Hong <23036686@myrp.edu.sg>
2025-01-11 13:53:05 +0800

    my changes

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,9 @@ import Home from "./Home";
 import About from "./About";
 import Categories from "./Categories";
 import Category from "./Category";
+import Session from "./Session";
+import Register from "./Register";
+import Confirmation from "./Confirmation";
 import Header from "./Header";
 
 function App() {
@@ -15,12 +18,14 @@ function App() {
         <Route path="/" element={<Home title="Welcome to Red30 Tech" />} />
         <Route path="about" element={<About />} />
         <Route path="categories" element={<Categories />}>
-          <Route path=":catId" element={<Category />} />
+          <Route path=":catId" element={<Category />}>
+            <Route path=":sessionId" element={<Session />} />
+            <Route index element={<h3>Select a category from above</h3>} />
+          </Route>
         </Route>
-        <Route
-          path="*"
-          element={<h1 className="not-found">Page Not Found</h1>}
-        />
+        <Route path="register" element={<Register />} />
+        <Route path="confirmed" element={<Confirmation />} />
+        <Route path="*" element={<h1>Page Not Found</h1>} />
       </Routes>
 
       <footer className="container">
